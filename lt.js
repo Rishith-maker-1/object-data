d4f314f23cc86fb7820dcac7e258a48af107ae44
Rishith-maker-1 <83953765+Rishith-maker-1@users.noreply.github.com>
2022-02-20 18:27:14 +0530

    Add files via upload

--- lt.js
+++ lt.js
@@ -1,5 +1,6 @@
 img="";
 Status="";
+objects=[];
 function setup(){
     canvas=createCanvas(640,420);
     canvas.center();
@@ -11,6 +12,17 @@ function preload(){
 }
 function draw(){
     image(img,0,0,640,420);
+    if(Status!=""){
+        for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="Status=Objects Detected";
+            fill("Red");
+            percent=floor(objects[i].cofidence*100);
+            text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("blue");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
 }
 function modelloaded(){
     console.log("Model Loaded");
@@ -22,6 +34,7 @@ function gotresults(error,results){
          console.error(error);
      }else{
          console.log(results);
+         objects=results;
      }
 }
 function back(){
